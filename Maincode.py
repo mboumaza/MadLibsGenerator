ec7c679d94de8422d0ba3b15be62924f5469931c
Mboumaza <mathis.arthur.boumaza@gmail.com>
2021-06-26 00:33:39 +0200

    Updates

---
+++ Maincode.py
@@ -0,0 +1,28 @@
+#To Do (Overall):
+#1) Random file full of texts with words missing 
+#2) Code to randomly generate a text from that file
+#3) Get the text and put it into a PDF file
+
+#Extra
+#4) Ask user name and date and eventually put it into the PDF file 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
