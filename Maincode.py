f46ca5bdf1f89ec945e204b76f72009ea1d6b05e
Mboumaza <mathis.arthur.boumaza@gmail.com>
2021-06-26 03:12:31 +0200

    Update

--- Maincode.py
+++ Maincode.py
@@ -6,8 +6,8 @@
 #Extra
 #4) Ask user name and date and eventually put it into the PDF file 
 
-
-
+name = input("Please give us your name: ")
+list_index = [list(range(1,10)), list(range(12,24)), list(range(26,40), list(range(42,64), list(range(66,79)), list(range(80,84)), list(range(86,89)), list(range(91,101)), list(range(103,111)), list(range(113,116))]
 
 
 
